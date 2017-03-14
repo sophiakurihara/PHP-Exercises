6c1afbef57c75b5b74069f0ef724e7c8d8cc944e
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-03-14 10:22:47 -0500

    is it supposed to be 7, or 5 if-blocks? I'm counting 5

---
+++ if_and_or.php
@@ -0,0 +1,29 @@
+<?php
+
+$x = 0;
+$y = 5;
+$z = 10;
+
+// TODO:
+// If $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+if ($x < $y && $y < $z) {
+	echo "{$x} < {$y} < {$z}\n";
+}
+// TODO:
+// If 0 is less than $x OR $x is less than 10
+// then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
+if (0 < $x || $x < 10) {
+	echo "0 is less than {$x} OR {$x} is less than 10";
+}
+// TODO:
+// repeat the if statement for $y and $z.
+if (0 < $y || $z < 10) {
+	echo "0 is less than {$y} OR {$z} is less than 10"
+}
+
+// TODO:
+// If 0 is less than $x AND $x is less than 10
+// then echo the result as a sentence "0 is less than {$x} AND {$x} is less than 10".
+
+// TODO:
+// repeat the if statement for $y and $z.
