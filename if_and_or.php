6f4697a1edfe6f97ec6b22d12dc457063c99b936
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-03-14 10:33:28 -0500

    it IS 7 boocks! i finally understand what this exercise is asking for

--- if_and_or.php
+++ if_and_or.php
@@ -17,8 +17,12 @@ if (0 < $x || $x < 10) {
 }
 // TODO:
 // repeat the if statement for $y and $z.
-if (0 < $y || $z < 10) {
+if (0 < $y || $y < 10) {
 	echo "0 is less than {$y} OR {$z} is less than 10\n";
+}
+
+if (0 < $z || $z < 10) {
+	echo "0 is less than {$z} OR {$z} is less than 10\n";
 }
 
 // TODO:
