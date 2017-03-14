21c88663a2c7581040a4b82fcd089b2388a8c996
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-03-14 15:18:28 -0500

    checking user input for correct types (int)

--- for.php
+++ for.php
@@ -8,8 +8,23 @@ fwrite(STDOUT, 'Feed me an ending number:' . PHP_EOL);
 
 $end = trim(fgets(STDIN) . PHP_EOL);
 
+fwrite(STDOUT, 'Count by what increment? Enter an integer:' . PHP_EOL);
+
+$increment = trim(fgets(STDIN) . PHP_EOL);
+
 fwrite(STDOUT, 'Counting...' . PHP_EOL);
 
-for ($i = $start; $i <= $end; $i++) {
+if(!is_numeric($start) || !is_numeric($end)) {
+	fwrite(STDOUT, "Please enter integers as arguments:" . PHP_EOL);
+} elseif (is_numeric($increment) && $increment > 0) {
+	for ($i = $start; $i <= $end; $i += $increment) {
 	echo "$i\n";
+	}
+} else {
+	for ($i = $start; $i <= $end; $i++) {
+	echo "$i\n";
+	}
+	fwrite(STDOUT, "Please only enter an integer for increment value." . PHP_EOL);
 }
+
+exit(0);
