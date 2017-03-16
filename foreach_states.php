0a0d62fc7fbacb360e94346dbf9176a3953ccda3
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-03-16 16:25:44 -0500

    part 2 of exercises in foreach_states. in progress

--- foreach_states.php
+++ foreach_states.php
@@ -83,3 +83,31 @@ foreach ($states as $key => $value) {
 	}
 
 }
+
+// Use a foreach to make a new array containing states that start with and end with vowels
+	// call the array $statesStartingAndEndingWithVowels
+	// echo "These are states starting and ending with vowels"
+	// then echo each state name in this new array
+foreach ($states as $key => $value) {
+	$startAndEnd = array();
+	
+	if (substr($value, -1) == 'a' || substr($value, -1) == 'e' || substr($value, -1) == 'i' || substr($value, -1) == 'o' || substr($value, -1) == 'u') {
+		if ($value[0] == 'A') {
+			echo "$value\n";
+			$startAndEnd.= $value;
+		} elseif ($value[0] == 'E') {
+			echo "$value\n";
+		} elseif ($value[0] == 'I') {
+			echo "$value\n";
+		} elseif ($value[0] == 'O') {
+			echo "$value\n";
+		} elseif ($value[0] == 'U') {
+			echo "$value\n";
+		} 
+	}
+}
+
+// use a foreach to construct a new array of all the states with "North" "East" "South" or "West"
+	// call the array $arrayOfCardinalStates
+	// echo "These are states with north, south, east, or west in their name"
+
