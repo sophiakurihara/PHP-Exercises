05958ad7d0612d228804d9bbfbfb7675dff00146
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-03-20 16:51:58 -0500

    created humanizedList function to pass array through with solution

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,4 +1,19 @@
 <?php
 
-$famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, and Tony Stark';
-echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}";
+function humanizedList($array) {
+array_pop($array);
+array_push($array, 'and Tony Stark.');
+
+$newArray = implode(', ', $array);
+
+return ($newArray);
+	
+}
+
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+$famousFakePhysicistsArray = explode(', ', $physicistsString);
+
+$famousFakePhysicists = humanizedList($famousFakePhysicistsArray);
+
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}" . PHP_EOL;
